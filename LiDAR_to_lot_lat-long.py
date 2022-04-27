65f3babde762de8f7ea7edabe6f6bade7845f8b8
slohar23 <83802451+slohar23@users.noreply.github.com>
2022-04-27 18:29:36 -0400

    Update LiDAR_to_lot_lat-long.py

--- LiDAR_to_lot_lat-long.py
+++ LiDAR_to_lot_lat-long.py
@@ -15,8 +15,7 @@ import utm
 
 ## Read in excel file and get heading, pos_x as numpy arrays:
 # note: change file path and name:
-# df = pd.read_excel ('/Users/shreyalohar/Desktop/pythonprac/park_garage/lat_long.xlsx')
-df = pd.read_csv ('/Users/shreyalohar/Desktop/Final_project/ERR4.csv')
+df = pd.read_csv ('Project.csv')
 # heading = pd.DataFrame(df, columns = ['heading']).to_numpy()
 # dist = pd.DataFrame(df, columns = ['dist']).to_numpy()
 
@@ -31,9 +30,10 @@ print ("Shape of xy_lidar pts is: ", xy_pts_lidar.shape)
 
 ## Define constants:
 R = 6378 #Radius of the Earth
-lat_origin =  35.307961   # Current lat point in degrees
-lon_origin = -80.732167   # Current long point in degrees
+lat_origin =  35.307961   # Current LiDAR lat point in degrees
+lon_origin = -80.732167   # Current LiDAR long point in degrees
 
+#Add heading angle of the LiDAR
 heading_lidar = np.radians(170)    # North is +90 deg, East is 0 deg
 
 ## Rotation matrix - LIDAR to UTM frame: 
@@ -67,37 +67,7 @@ df['lat_out'] = np.squeeze(lat_out)
 df['lon_out'] = np.squeeze(lon_out)
 
 ## Write to excel file:
-df.to_csv('Parking_lot_error4.csv')
+df.to_csv('New_LiDAR_lat_long_data.csv')
 
-
-
-
-######################## OLD CODE ##########################################
-# ## Calculate lat2 and lon2:
-# lat2 = np.degrees(np.arcsin(np.sin(lat1) * np.cos(pos_x/R) + np.cos(lat1) * np.sin(pos_x/R) * np.cos(heading)))
-
-# lon2 = np.degrees(lon1 +  np.arctan2( np.sin(heading) * np.sin(pos_x/R) * np.cos(lat1), np.cos(pos_x/R) - np.sin(lat1) * np.sin(lat2)))
-
-# ## Add lat2 and lon2 to the data frame:
-# df['lat2'] = np.squeeze(lat2)
-# df['lon2'] = np.squeeze(lon2)
-
-# ## Write to excel file:
-# df.to_excel('lat_long_new.xls')
-
-
-## Old code for ref:
-
-#lat2 = math.asin( math.sin(lat1)*math.cos(d/R) +
-             #math.cos(lat1)*math.sin(d/R)*math.cos(brng))
-
-#lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),
-                     #math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
-#lat2 = math.degrees(lat2)
-#lon2 = math.degrees(lon2)
-
-# df['lat2'] = np.degrees(np.arcsin(np.sin(lat1)*np.cos(df['dx']/R) + np.cos(lat1)*np.sin(df['dx']/R)*np.cos(df['heading_rad'])))
-# df['lat2'].astype(float)
-# df['lon2'] = np.degrees(lon1 + np.arctan(np.sin(df['heading_rad'])*np.sin(df['dx']/R)*np.cos(lat1),np.cos(df['dx']/R)-np.sin(lat1)*np.sin(df['lat2'])))
 
 
